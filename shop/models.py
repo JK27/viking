20a19f2a1840a83145d7c3132f64cd0b3d53f593
Juan C. Martin <juankarmartin20@gmail.com>
2020-07-12 12:40:43 +0000

    Customized products admin

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+# ------------------------------------------ CATEGORIES
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+# ------------------------------------------ PRODUCTS
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    discounted_price = models.DecimalField(max_digits=6, decimal_places=2,
+                                           null=True)
+    brand = models.CharField(max_length=20, default="")
+    gender = models.CharField(max_length=10, default="")
+    base_colour = models.CharField(max_length=20, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
