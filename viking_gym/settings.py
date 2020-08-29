3d09a935ec0397c7292b27b8a29111a48a169a65
Juan C. Martin <juankarmartin20@gmail.com>
2020-08-29 18:15:03 +0000

    Removed secret key and reset debug

--- viking_gym/settings.py
+++ viking_gym/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY', '')
-SECRET_KEY = '&qvc#3n$w99hamin!ez6w+52cmxss*@nq3l_i-+4c(1n-ztvkq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
