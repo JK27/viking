f23be38d0250fd5b22c319e8430a730355844e45
Juan C. Martin <juankarmartin20@gmail.com>
2020-08-31 11:13:53 +0000

    Amend authentication backends

--- viking_gym/settings.py
+++ viking_gym/settings.py
@@ -106,13 +106,13 @@ TEMPLATES = [
 # Required to allow messages to be stored during the session
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-AUTHENTICATION_BACKENDS = [
+AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-]
+)
 
 SITE_ID = 1
 
